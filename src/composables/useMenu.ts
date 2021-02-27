f62f2b79cdd0e264c4090f468a1c79635b37c045
Nekilc <nieaowei@gmail.com>
2021-02-27 14:02:31 +0800

    Update useMarkdown to make it more reasonable.

--- src/composables/useMenu.ts
+++ src/composables/useMenu.ts
@@ -3,7 +3,7 @@ import { useRouter} from "vue-router";
 export function useMenu() {
     const {getRoutes} = useRouter()
     const menuRouters = getRoutes().filter((value, index) => {
-        return !!value.meta.isMenu;
+        return !!value.meta?.isMenu;
     })
     return {
         menuRouters
