c5848af99d7c92f7bc57a697a29bc23c076825db
Nekilc <nieaowei@gmail.com>
2021-03-02 23:14:20 +0800

    remove antd-ui

--- src/composables/useRegisterForm.ts
+++ src/composables/useRegisterForm.ts
@@ -1,5 +1,5 @@
 import {reactive} from 'vue'
-import {useForm} from "@ant-design-vue/use";
+
 
 
 export default function useRegisterForm() {
@@ -16,68 +16,8 @@ export default function useRegisterForm() {
         }
         return Promise.resolve();
     };
-    const {validate, validateInfos} = useForm(
-        modelRef,
-        reactive({
-            schoolNumber: [
-                {
-                    required: true, trigger: ['change', 'blur'], message: '请输入学号',
-                },
-                {
-                    len: 11, trigger: ['change', 'blur'], message: '学号长度不正确',
-                },
-            ],
-            password: [
-                {
-                    required: true, trigger: ['change', 'blur'], message: '请输入密码',
-                },
-                {
-                    pattern: '(?=.*?[a-zA-Z])(?=.*?[0-9])(?=.{6,14})',
-                    trigger: ['change', 'blur'],
-                    message: '请输入至少包括字母和数字的组合',
-                },
-                {
-                    validator: async (rule: string, value: string, callback: any) => {
-                        if (value !== '') {
-                            validate('confirm')
-                        }
-                        return Promise.resolve();
-                    }, trigger: ['change', 'blur']
-                },
-            ],
-            confirm: [
-                {
-                    required: true, trigger: ['change', 'blur'], message: '请再次输入密码',
-                },
-                {
-                    validator: validateConfirm, trigger: ['change', 'blur']
-                },
-            ],
-            phone: [
-                {
-                    required: true, trigger: ['change', 'blur'], message: '请输入手机号码',
-                },
-                {
-                    pattern: '\\d{11}',
-                    trigger: ['change', 'blur'],
-                    message: '手机号码格式不正确',
-                    transform: (val: number) => val.toString()
-                },
-            ],
-            code: [
-                {
-                    required: true, trigger: ['change', 'blur'], message: '请输入验证码',
-                },
-            ],
-        }),
-    );
-    const onSubmit = (e: { preventDefault: () => void }) => {
-        e.preventDefault();
-        return validate()
-    };
+
     return {
-        validateInfos,
         modelRef,
-        onSubmit,
     };
 }
