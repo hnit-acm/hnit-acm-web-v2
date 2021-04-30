9bead2e150f1def63b266da70573be77693d4ed8
Nekilc <nieaowei@gmail.com>
2021-04-30 15:36:13 +0800

    ✨ feat: About page

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -1,16 +1,25 @@
 <script lang="ts" setup>
 import { onMounted } from 'vue'
 import { usePageBannerInject } from "@/composables/Home/usePageBanner";
-import { useBreadcrumbInject } from "@/composables/Home/useBreadcrumb";
+import { useMarkdown } from '@/composables/useMarkdown';
+import PageLayout from '@/views/layout/PageLayout.vue';
+import { pageLayout } from '@/views/layout/layout';
 
-const { setVisible } = useBreadcrumbInject()
-onMounted(() => {
-  setVisible(false)
-})
+const { content, refresh } = useMarkdown('')
+refresh("# 湖南工学院ACM队\n\n\n欢迎加入")
+
 usePageBannerInject()
 </script>
 
 <template lang="pug">
-.about
-  img(style="width: '20%';height: '20%'" src="../../assets/nav-logo-color.png")/
+page-layout(v-bind:layout="pageLayout")
+  el-col()
+    img(style="width: '20%';height: '20%'" src="../../assets/about-logo.png")
+  el-col()
+    div(v-html="content")
+    el-button-group
+      el-button() 软件组
+      el-button() 算法组
+      el-button() 机器人
+
 </template>
