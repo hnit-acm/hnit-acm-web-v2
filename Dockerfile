30d7bbcf4924e088b39f47d1f8608fe842cba751
Nekilc <nieaowei@gmail.com>
2021-07-03 15:56:26 +0800

    💚 fix: fix build

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY . /home/app
 
 WORKDIR /home/app
 
-RUN ls && mv ./h3.nginx.conf /etc/nginx/nginx.conf
+RUN ls && mv ./nginx.conf /etc/nginx/nginx.conf
 
 ENTRYPOINT ["nginx","-g","daemon off;"]
--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ npm run build
 cp nginx.conf ./dist/
 cp h3.nginx.conf ./dist/
 cp hnit-acm.nekilc.cn.key ./dist/
-cp hnit-acm.nekilc.cn.epm ./dist/
+cp hnit-acm.nekilc.cn.pem ./dist/
 cp Dockerfile ./dist/
 ls .
